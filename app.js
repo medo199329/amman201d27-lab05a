70d6c06d91d5f5e234f48b84a1a986b1cba230dd
medo199329 <mohammeddeeb747@gmail.com>
2021-05-27 15:38:52 +0300

    new answer

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) {
-  //eslint-disable-line
+  let x =
+  
 }
-
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
 
