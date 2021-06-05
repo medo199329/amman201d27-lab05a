aed5bc359f25cb6a6b503e81415e72a6df2e1126
medo199329 <mohammeddeeb747@gmail.com>
2021-06-05 19:39:43 +0300

    problem1 solved

--- app.js
+++ app.js
@@ -108,6 +108,8 @@ Test this function by hand in the console to get it working, and when you think 
 /* STRETCH GOAL: Problem 6
 Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
 
+
+
 "The numbers 1,2,3,4,5 have a product of 120."
 
 IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
